0d3cc783fe1abf6d98ce93971fa507fe09cbbc46
Amir Hossein Nourbakhsh <nou2177@Tamco.Eng>
2023-11-22 09:17:57 +0330

    developed search bar

--- src/components/HeaderComponent/HeaderComponent.svelte
+++ src/components/HeaderComponent/HeaderComponent.svelte
@@ -1,12 +1,17 @@
 <script>
-    import image from '../../images/pattern-bg-desktop.png';
-  </script>
-  
-  <div class="Header">
-    <img src="{image}" class="hidden" alt="Pattern Background"/>
-  </div>
-  
-  <style>
+    import image from "../../images/pattern-bg-desktop.png";
+    import SearchBarComponent from "../SearchBarComponent/SearchBarComponent.svelte";
+</script>
 
-  </style>
-  
+<div class="Header relative">
+    <p class="absolute text-3xl left-1/2 -translate-x-1/2 top-6 font-semibold">
+        IP Address Tracker
+    </p>
+    <img src={image} class="w-full" alt="Pattern Background" />
+    <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-4/12">
+        <SearchBarComponent />
+    </div>
+</div>
+
+<style>
+</style>
---
+++ src/components/SearchBarComponent/SearchBarComponent.svelte
@@ -0,0 +1,19 @@
+<script>
+    import arrow from "../../images/icon-arrow.svg";
+</script>
+
+<div class="SearchBarComponent relative">
+    <input
+        placeholder="Search for any IP address or domain"
+        class="w-full bg-white rounded-xl p-4 outline-none text-black"
+    />
+
+    <div
+        class="absolute bg-black w-[12%] right-0 rounded-r-xl top-0 h-full flex flex-col items-center justify-center"
+    >
+        <img src={arrow} alt="arrow" />
+    </div>
+</div>
+
+<style>
+</style>
