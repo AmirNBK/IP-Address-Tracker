10b139ea75259ca373a8acff84df25d9ce0a488d
Amir Hossein Nourbakhsh <nou2177@Tamco.Eng>
2023-11-22 12:11:53 +0330

    developed Info component

--- src/components/HeaderComponent/HeaderComponent.svelte
+++ src/components/HeaderComponent/HeaderComponent.svelte
@@ -1,5 +1,6 @@
 <script>
     import image from "../../images/pattern-bg-desktop.png";
+    import InfoComponent from "../InfoComponent/InfoComponent.svelte";
     import SearchBarComponent from "../SearchBarComponent/SearchBarComponent.svelte";
 </script>
 
@@ -8,8 +9,19 @@
         IP Address Tracker
     </p>
     <img src={image} class="w-full" alt="Pattern Background" />
-    <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-4/12">
+    <div
+        class="absolute left-1/2 top-1/3 -translate-x-1/2 -translate-y-1/2 w-4/12"
+    >
         <SearchBarComponent />
+    </div>
+
+    <div class="w-full absolute top-2/3">
+        <InfoComponent
+            IP="192.212.23.451"
+            Location="Brooklyn, NY"
+            Timezone="05:00"
+            ISP="SpaceX"
+        />
     </div>
 </div>
 
---
+++ src/components/InfoComponent/InfoComponent.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  export let IP;
+  export let Location;
+  export let Timezone;
+  export let ISP;
+
+  const infos = [
+    {
+      title: "IP Address",
+      info: IP,
+    },
+    {
+      title: "Location",
+      info: Location,
+    },
+    {
+      title: "Timezone",
+      info: `UTC - ${Timezone}`,
+    },
+    {
+      title: "ISP",
+      info: ISP,
+    },
+  ];
+</script>
+
+<div
+  class="InfoComponent bg-white rounded-lg flex flex-row text-black w-8/12 p-12 mx-auto flex flex-row justify-around gap-6"
+>
+  {#each infos as item}
+    <div
+      class="flex flex-col items-start pl-6 border-l border-solid border-[#D0D4CA] first:border-none gap-2"
+    >
+      <p class="text-[#9A9A9A] font-bold">{item.title}</p>
+      <p class="text-2xl font-bold">{item.info}</p>
+    </div>
+  {/each}
+</div>
+
+<style>
+</style>
