eb0e1b85af6189054a5a6e94a303f754836e9d71
Amir Hossein Nourbakhsh <nou2177@Tamco.Eng>
2023-11-22 08:30:34 +0330

    added tailwind configs and some assets to the project

---
+++ src/components/HeaderComponent/HeaderComponent.svelte
@@ -0,0 +1,12 @@
+<script>
+    import image from '../../images/pattern-bg-desktop.png';
+  </script>
+  
+  <div class="Header">
+    <img src="{image}" class="hidden" alt="Pattern Background"/>
+  </div>
+  
+  <style>
+
+  </style>
+  
